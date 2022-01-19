cf1fc11fde6062988e33a446110c61b0367ed6ba
Aline <eualine2017@gmail.com>
2022-01-19 17:41:23 -0300

    Req4 CORRIGIDO

--- middleware/createTalker.js
+++ middleware/createTalker.js
@@ -9,18 +9,18 @@ const validateToken = (req, res, next) => {
 
 const createTalker = (req, res, next) => {
     const { name } = req.body;
-    if (!name) return res.status(400).json({ mensage: 'O campo "name" é obrigatório' });
-    if (name < 3) {
-        return res.status(400).json({ mensage: 'O "name" deve ter pelo menos 3 caracteres' });
+    if (!name) return res.status(400).json({ message: 'O campo "name" é obrigatório' });
+    if (name.length < 3) {
+        return res.status(400).json({ message: 'O "name" deve ter pelo menos 3 caracteres' });
         }
     next();
 };
 
 const createAge = (req, res, next) => {
     const { age } = req.body;
-    if (!age) return res.status(400).json({ mensage: 'O campo "age" é obrigatório' });
+    if (!age) return res.status(400).json({ message: 'O campo "age" é obrigatório' });
     if (age < 18) {
-        return res.status(400).json({ mensage: 'A pessoa palestrante deve ser maior de idade' });
+        return res.status(400).json({ message: 'A pessoa palestrante deve ser maior de idade' });
         }
     next();
 };
@@ -40,11 +40,11 @@ const createTalk = (req, res, next) => {
     const validDate = reg.test(watchedAt);
     if (!validDate) {
         return res.status(400).json({
-            mensage: 'O campo "watchedAt" deve ter o formato "dd/mm/aaaa' });
+            message: 'O campo "watchedAt" deve ter o formato "dd/mm/aaaa' });
     }
     if (rate < 1 || rate > 5) {
         return res.status(400).json({
-            mensage: 'O campo "rate" deve ser um inteiro de 1 à 5' });
+            message: 'O campo "rate" deve ser um inteiro de 1 à 5' });
     }
     next();
 };
